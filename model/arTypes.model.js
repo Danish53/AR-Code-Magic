858af49e34548a456fe37e40b9105af7b4b65ec4
Danish53 <daynish05@gmail.com>
2025-02-13 13:33:32 +0500

    Initial commit three

--- model/arTypes.model.js
+++ model/arTypes.model.js
@@ -65,7 +65,7 @@ const ArTypes = sequelize.define(
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "Users",
+        model: "users",
         key: "id",
       },
     },
